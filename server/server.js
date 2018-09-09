a79d7d93cc62e6a7bec72f473c822a0e9008b20e
Prashant Godhwani <prashant.godhwani@encodezero.com>
2018-09-09 18:55:47 +0530

    added /GET todos

--- server/server.js
+++ server/server.js
@@ -14,15 +14,20 @@ app.post('/todos', (req, res) => {
   var todo  = new Todo({
     text : req.body.text
   });
-
-
-
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
   })
 });
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.status(200).send({todos})
+  }, (e) => {
+      res.status(400).send(e);
+    });
+  });
 
 
 
