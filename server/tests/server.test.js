5812efc0fa126d1f5aab899ab053b7301c649d65
Prashant Godhwani <prashant.godhwani@encodezero.com>
2018-09-09 19:17:36 +0530

    added test /GET todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,13 +4,22 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [
+  {
+    text : "First test todo"
+  },
+  {
+    text : "Second test case"
+  }
+];
 
 beforeEach((done) => {
-  Todo.remove({}).then(done());
-})
+  Todo.remove({}).then(() => {
+      return Todo.insertMany(todos);
+  }).then(() => done());
+});
 
 describe('/POST todos', () => {
-
   it('should create a new todo', (done) => {
     var text = "should clear the test";
     request(app)
@@ -24,15 +33,14 @@ describe('/POST todos', () => {
             if(err){
               return done(err);
             }
-            Todo.find().then((todos) => {
+            Todo.find({text}).then((todos) => {
               expect(todos.length).toBe(1);
               expect(todos[0].text).toBe(text);
               done();
             }).catch((e) => done(e));
           });
   });
-
-it('should return an error for invalid todo', (done) => {
+  it('should return an error for invalid todo', (done) => {
     request(app)
             .post('/todos')
             .send({})
@@ -43,10 +51,23 @@ it('should return an error for invalid todo', (done) => {
               }
 
               Todo.find().then((todos) => {
-                  expect(todos.length).toBe(0);
+                  expect(todos.length).toBe(2);
                   done();
               }).catch((e) => done(e));
             });
+      });
 });
 
+describe('/GET todos', () => {
+    it('should get all todos', (done) => {
+      request(app)
+              .get('/todos')
+              .expect(200)
+              .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+              })
+              .end((err) => {
+                  done(err);
+              });
+    });
 });
