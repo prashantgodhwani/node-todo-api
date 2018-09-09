523337f6600eebaee8179b9acb9d88c147083bf2
Prashant Godhwani <prashant.godhwani@encodezero.com>
2018-09-09 21:53:02 +0530

    Added /GET todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose.js');
 const {Todo} = require('./models/todo.js');
 const {User} = require('./models/user.js');
@@ -27,6 +28,20 @@ app.get('/todos', (req, res) => {
   }, (e) => {
       res.status(400).send(e);
     });
+  });
+
+  app.get('/todos/:id', (req, res) => {
+    if(!ObjectID.isValid(req.params.id)){
+      return res.status(404).send({'Error' : 'ObjectID Invalid.'});
+    }
+    Todo.findById(req.params.id).then((todo) => {
+      if(!todo){
+        return res.status(404).send({
+          'Error' : 'ObjectID not found.'
+        });
+      }
+      res.status(200).send({todo});
+    }).catch((e) => res.status(400).send({'Error' : 'Error in finding todo.'}));
   });
 
 
