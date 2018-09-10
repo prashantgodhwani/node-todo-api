355f5b9eeef069e9cddd0b4ebccb3bf97fd0405f
Prashant Godhwani <prashant.godhwani@encodezero.com>
2018-09-10 08:39:12 +0530

    Added test cases for /GET todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,14 +1,17 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [
   {
+    _id : new ObjectID(),
     text : "First test todo"
   },
   {
+    _id : new ObjectID(),
     text : "Second test case"
   }
 ];
@@ -69,5 +72,31 @@ describe('/GET todos', () => {
               .end((err) => {
                   done(err);
               });
+    });
+  });
+
+describe('/GET todos/:id', () => {
+    it('should get todo by :id', (done) => {
+          request(app)
+                 .get(`/todos/${todos[0]._id.toHexString()}`)
+                 .expect(200)
+                 .expect((res) => {
+                   expect(res.body.todo.text).toBe(todos[0].text)
+                 })
+                 .end(done);
+    });
+
+    it('should return a 404 if todo not found', (done) => {
+        request(app)
+                .get(`/todos/${new ObjectID().toHexString()}`)
+                .expect(404)
+                .end(done)
+    })
+
+    it('should return a 404 if invalid ObjectID', (done) => {
+        request(app)
+                .get('/todos/123abc')
+                .expect(404)
+                .end(done);
     });
 });
